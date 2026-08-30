0ffe889d54ac85db9a1f26a37343d3dff05cbb29
Raunit Shrivastava <raunit.shrivastava29@gmail.com>
2026-08-30 17:35:03 +0530

    parser impl

--- RJSONParser.cpp
+++ RJSONParser.cpp
@@ -14,61 +14,268 @@ RJSONParser::~RJSONParser ()
 bool
 RJSONParser::Parse ()
 {
-        eJSONTokenType curr_token = InternalGetCurrTokenType ();
+    InternalReadAndAdvance ();
 
-    if (curr_token == JSON_UNKNOWN)
+    if (vTokenType != eJSONTokenType::JSON_LBRACE)
     {
         return false;
     }
 
-    if (curr_token == JSON_EOF)
-    {
-        return true;
-    }
-
-    // Must have an object at root level
-    if (curr_token != JSON_LBRACE)
-    {
-        return false;
-    }
-
-    InternalParseObject ();
+    vRootObj = InternalParseObject ();
 
     return true;
 }
 
-eJSONTokenType
-RJSONParser::InternalGetCurrTokenType ()
+void
+RJSONParser::InternalReadAndAdvance ()
 {
         int c;
         char ch;
 
     c = vLoader->ReadByteAndAdvance ();
 
+    InternalSkipWhiteSpaces (c);
+
     if (c == EOF)
     {
-        return JSON_EOF;
+        vTokenType = eJSONTokenType::JSON_EOF;
+        return;
     }
 
     ch = (char) c;
+    vToken = ch;
 
-    // if (!gTokenFromLiteral.contains (ch))
-    // {
-    //     return JSON_UNKNOWN;
-    // }
+    if (vToken >= '0' && vToken <= '9')
+    {
+        vTokenType = eJSONTokenType::JSON_NUMBER;
+        return;
+    }
+    
+    if (gTokenFromLiteral.find (ch) == gTokenFromLiteral.end ())
+    {
+        vTokenType = JSON_UNKNOWN;
+        return;
+    }
 
-    // return gTokenFromLiteral [ch];
-    return JSON_EOF;
+    vTokenType = gTokenFromLiteral [ch];
 }   
 
-bool
+RJSONObject*
 RJSONParser::InternalParseObject ()
 {
+        RJSONObject* obj = new RJSONObject ();
+        bool rc;
 
+    rc = InternalParseObjectMembers (obj);
+
+    if (rc)
+        return obj;
+
+    return nullptr;
 }
 
 bool
-RJSONParser::InternalParseObjectMembers ()
+RJSONParser::InternalParseObjectMembers (RJSONObject* pObj)
 {
+    InternalReadAndAdvance ();
 
+    if (vTokenType != eJSONTokenType::JSON_QUOTE)
+    {
+        return false;
+    }
+
+    while (vToken == gLiteralFromToken [eJSONTokenType::JSON_QUOTE])
+    {
+        InternalParseObjectMember (pObj);
+        
+        InternalReadAndAdvance ();
+        
+        // Comma
+        if (vToken == gLiteralFromToken [eJSONTokenType::JSON_COMMA])
+        {
+            continue;
+        }
+
+        if (vToken == gLiteralFromToken [eJSONTokenType::JSON_RBRACE])
+        {
+            // Obj ended
+            return true;
+        }
+    }
+
+    InternalReadAndAdvance ();
+
+    return vToken == gLiteralFromToken [eJSONTokenType::JSON_RBRACE];
+}
+
+void
+RJSONParser::InternalParseObjectMember (RJSONObject* pObj)
+{
+        RJSONMember* kv = new RJSONMember ();
+
+    kv->uKey = InternalParseString ();
+
+    InternalReadAndAdvance ();
+    // Colon
+    if (vToken != gLiteralFromToken [eJSONTokenType::JSON_COLON])
+    {
+        return;
+    }
+
+    InternalReadAndAdvance ();
+
+    kv->uValue = InternalParsemMemberValue ();
+
+    pObj->uMembers.push_back (kv);
+}
+
+RJSONValue*
+RJSONParser::InternalParsemMemberValue ()
+{
+        RJSONValue* val = new RJSONValue ();
+    
+    if (vTokenType == eJSONTokenType::JSON_QUOTE)
+    {
+        // String val
+        val->uType = eRJSONValueType::RJSON_STR_VAL;
+        val->uStringVal = InternalParseString ();
+        
+        return val;
+    }
+
+    if (vTokenType == eJSONTokenType::JSON_NUMBER)
+    {
+        // num val
+        val->uType = eRJSONValueType::RJSON_NUM_VAL;
+        val->uNumVal = InternalParseNumber ();
+
+        return val;
+    }
+
+    if (vTokenType == eJSONTokenType::JSON_FALSE)
+    {
+        // bool val
+        val->uType = eRJSONValueType::RJSON_BOOL_VAL;
+        val->uBoolVal = false;
+
+        return val;
+    }
+
+    if (vTokenType == eJSONTokenType::JSON_TRUE)
+    {
+        // bool val
+        val->uType = eRJSONValueType::RJSON_BOOL_VAL;
+        val->uBoolVal = true;
+        
+        return val;
+    }
+
+    if (vTokenType == eJSONTokenType::JSON_LBRACE)
+    {
+        // obj val
+        val->uType = eRJSONValueType::RJSON_OBJECT_VAL;
+        val->uObjVal = InternalParseObject ();
+
+        return val;
+    }
+
+    if (vTokenType == eJSONTokenType::JSON_LBRACKET)
+    {
+        // array val
+        val->uType = eRJSONValueType::RJSON_ARRAY_VAL;
+        val->uArrayVal = new RJSONArray ();
+
+        while (vTokenType != eJSONTokenType::JSON_RBRACKET)
+        {
+            val->uArrayVal->uElements.push_back (InternalParsemMemberValue ());
+            
+            InternalReadAndAdvance ();
+
+            // if (vToken == gLiteralFromToken [eJSONTokenType::JSON_COMMA])
+            // {
+            //     continue;
+            // }
+        }
+
+        return val;
+    }
+
+    // todo null
+}
+
+RString*
+RJSONParser::InternalParseString ()
+{
+        RString* str = new RString ("");
+
+    do 
+    {
+        InternalReadAndAdvance ();
+
+        if (vToken != gLiteralFromToken [eJSONTokenType::JSON_QUOTE])
+            str->Append (vToken);
+    }
+    while (vToken != gLiteralFromToken [eJSONTokenType::JSON_QUOTE]);
+
+    return str;
+}
+
+double
+RJSONParser::InternalParseNumber ()
+{
+        double res = 0.0;
+        double sign = 1.0;
+        double fraction = 0.1;
+
+    if (vToken == '-')
+    {
+        sign = -1.0;
+    } 
+    else {
+
+        res = (double)(vToken - '0');
+    }
+
+    while (true)
+    {
+        InternalReadAndAdvance ();
+
+        if (vToken >= '0' && vToken <= '9')
+        {
+            res = (res*10.0) + (vToken - '0');
+        }
+        else {
+            break;
+        }
+    }
+
+    if (vToken == '.')
+    {
+        while (true)
+        {
+            InternalReadAndAdvance ();
+
+            if (vToken >= '0' && vToken <= '9')
+            {
+                res += (vToken - '0') * fraction;
+                fraction *= 0.1; 
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    InternalReadAndAdvance ();
+
+    return res * sign;
+}
+
+void
+RJSONParser::InternalSkipWhiteSpaces (int& pC)
+{
+    while (pC == '\n' || pC == ' ' || pC == '\t' || pC == '\r')
+    {
+        pC = vLoader->ReadByteAndAdvance ();
+    }
 }
--- includes/RJSONParser.hpp
+++ includes/RJSONParser.hpp
@@ -4,6 +4,8 @@
 #include "RFileUtilsLib.hpp"
 #include "RJSONLexer.hpp"
 
+#include <vector>
+
 enum eJSONTokenType 
 {
     JSON_LBRACE,
@@ -21,15 +23,118 @@ enum eJSONTokenType
     JSON_EOF
 };
 
-// std::unordered_map<RString, eJSONTokenType> gTokenFromLiteral = {
-//     {"{",   JSON_LBRACE},
-//     {"}",   JSON_RBRACE},
-//     {"[",   JSON_LBRACKET},
-//     {"]",   JSON_RBRACKET},
-//     {",",   JSON_COMMA},
-//     {":",   JSON_COLON},
-//     {"\"",  JSON_QUOTE},
-// };
+std::unordered_map<eJSONTokenType, RString> gTokenName =
+{
+    {JSON_LBRACE    , "JSON_LBRACE"},
+    {JSON_RBRACE    , "JSON_RBRACE"},
+    {JSON_LBRACKET  , "JSON_LBRACKET"},
+    {JSON_RBRACKET  , "JSON_RBRACKET"},
+    {JSON_COMMA     , "JSON_COMMA"},
+    {JSON_COLON     , "JSON_COLON"},
+    {JSON_QUOTE     , "JSON_QUOTE"},
+    {JSON_NUMBER    , "JSON_NUMBER"},
+    {JSON_TRUE      , "JSON_TRUE"},
+    {JSON_FALSE     , "JSON_FALSE"},
+    {JSON_NULL      , "JSON_NULL"},
+    {JSON_UNKNOWN   , "JSON_UNKNOWN"},
+    {JSON_EOF       , "JSON_EOF"}
+};
+
+std::unordered_map<char, eJSONTokenType> gTokenFromLiteral = 
+{
+    {'{',   JSON_LBRACE},
+    {'}',   JSON_RBRACE},
+    {'[',   JSON_LBRACKET},
+    {']',   JSON_RBRACKET},
+    {',',   JSON_COMMA},
+    {':',   JSON_COLON},
+    {'"',   JSON_QUOTE},
+    {'f',   JSON_FALSE},
+    {'t',   JSON_TRUE},
+    {'n',   JSON_NULL},
+};
+
+std::unordered_map<eJSONTokenType, char> gLiteralFromToken = 
+{
+    {JSON_LBRACE,   '{',},
+    {JSON_RBRACE,   '}',},
+    {JSON_LBRACKET, '[',},
+    {JSON_RBRACKET, ']',},
+    {JSON_COMMA,    ',',},
+    {JSON_COLON,    ':',},
+    {JSON_QUOTE,    '"'},
+};
+
+enum eRJSONValueType 
+{
+    RJSON_OBJECT_VAL,
+    RJSON_ARRAY_VAL,
+    RJSON_BOOL_VAL,
+    RJSON_NUM_VAL,
+    RJSON_STR_VAL,
+    RJSON_NULL_VAL
+};
+
+struct RJSONValue;
+struct RJSONObject;
+struct RJSONMember;
+
+struct RJSONObject
+{
+    std::vector<RJSONMember*> uMembers;
+};
+
+struct RJSONArray 
+{
+    std::vector<RJSONValue*> uElements;
+};
+
+struct RJSONValue
+{
+    eRJSONValueType          uType;
+    RString*                 uStringVal  = nullptr;
+    double                   uNumVal     = 0.0;
+    bool                     uBoolVal    = false;
+    RJSONObject*             uObjVal     = nullptr;
+    RJSONArray*              uArrayVal   = nullptr;
+
+    ~RJSONValue ()
+    {
+        if (uObjVal)
+        {
+            delete uObjVal;
+        }
+
+        if (uArrayVal)
+        {
+            delete uArrayVal;
+        } 
+
+        if (uStringVal)
+        {
+            delete uStringVal;
+        }
+    }
+};
+
+struct RJSONMember
+{
+    RString*                uKey = nullptr;
+    RJSONValue*             uValue = nullptr;
+
+    ~RJSONMember ()
+    {
+        if (uValue)
+        {
+            delete uValue;
+        }
+
+        if (uKey)
+        {
+            delete uKey;
+        }
+    }
+};
 
 class RJSONParser {
 
@@ -42,10 +147,18 @@ public:
 
 private:
 
-    eJSONTokenType  InternalGetCurrTokenType    ();
-    bool            InternalParseObject         ();
-    bool            InternalParseObjectMembers  ();
+    void            InternalReadAndAdvance      ();
+    RJSONObject*    InternalParseObject         ();
+    bool            InternalParseObjectMembers  (RJSONObject* pObj);
+    void            InternalSkipWhiteSpaces     (int& pC);
+    void            InternalParseObjectMember   (RJSONObject* pObj);
+    RJSONValue*     InternalParsemMemberValue   ();
+    RString*        InternalParseString         ();
+    double          InternalParseNumber         ();
 
     RLoader*        vLoader;
     RJSONLexer*     vLexer;
+    char            vToken;
+    eJSONTokenType  vTokenType;
+    RJSONObject*    vRootObj;
 };
